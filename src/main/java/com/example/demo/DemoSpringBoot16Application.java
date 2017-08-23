821a25a0be4c610bbdbba8251705e1004fa4419a
kikorose <nakelia78@gmil.com>
2017-08-23 16:02:57 -0400

    first commit(using page fragments)

---
+++ src/main/java/com/example/demo/DemoSpringBoot16Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot16Application.class, args);
+	}
+}
